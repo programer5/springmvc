1b8f699c68e38ab7fbcf149b26542257166f66bc
minseo23 <neverdie4757@gmail.com>
2021-07-06 23:36:41 +0900

    HTTP 요청 - 기본, 헤더 조회

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
+// import lombok.extern.slf4j.Slf4j;
 
 // @Slf4j
 @RestController
